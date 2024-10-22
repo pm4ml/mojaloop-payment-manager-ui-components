34e71ecf8310fad27e93151f419e8010e2d4dfc1
Chris Law <62750125+chris-me-law@users.noreply.github.com>
2024-10-22 15:03:13 +0100

    Update eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -1,88 +1,92 @@
-module.exports = {
-  parser: '@babel/eslint-parser',  // Correct usage as a string
-  parserOptions: {
-    ecmaVersion: 2020,
-    sourceType: 'module',
-    ecmaFeatures: {
-      jsx: true,
-    },
-    requireConfigFile: false,
-  },
-  env: {
-    browser: true,
-    node: true,
-    es6: true,
-  },
-  plugins: [
-    'react',
-    'jest',
-    'import',
-    'jsx-a11y',
-  ],
-  settings: {
-    react: {
-      version: 'detect',
-    },
-  },
-  extends: [
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:jsx-a11y/recommended',
-    'plugin:import/errors',
-    'plugin:import/warnings',
-    'plugin:jest/recommended',
-    'prettier',
-  ],
-  rules: {
-    ...require('eslint-config-airbnb-base').rules,
-    ...require('eslint-config-prettier').rules,
+import babelParser from '@babel/eslint-parser';
+import eslintConfigAirbnbBase from 'eslint-config-airbnb-base';
+import eslintConfigPrettier from 'eslint-config-prettier';
+import reactPlugin from 'eslint-plugin-react';
+import jestPlugin from 'eslint-plugin-jest';
+import importPlugin from 'eslint-plugin-import';
+import jsxA11yPlugin from 'eslint-plugin-jsx-a11y';
 
-    // Custom rules
-    'react/jsx-key': 'off', 
-    'jsx-a11y/click-events-have-key-events': 'off',
-    'jsx-a11y/no-noninteractive-element-to-interactive-role': 'off',
-    'react/destructuring-assignment': 'off',
-    'no-unused-vars': 'off',
-    'react/display-name': 'off',
-    'import/namespace': 'off',  
-    'max-len': 'off',
-    'sort-imports': 'off',
-    'import/order': 'off',
-    'import/first': 'off',
-    quotes: ['error', 'single', { allowTemplateLiterals: true }],
-    indent: ['error', 2, { SwitchCase: 1 }],
-    'no-underscore-dangle': 'off',
-    'import/no-extraneous-dependencies': [
-      'error',
-      {
-        devDependencies: true,
-        optionalDependencies: false,
-        peerDependencies: false,
+export default [
+  {
+    files: ['**/*.js', '**/*.jsx'], // Apply to JavaScript and JSX files
+    languageOptions: {
+      parser: babelParser, // Set Babel as the parser
+      parserOptions: {
+        ecmaVersion: 2020,
+        sourceType: 'module',
+        ecmaFeatures: {
+          jsx: true, // Enable JSX parsing
+        },
+        requireConfigFile: false,
       },
-    ],
-    'react/prefer-stateless-function': ['warn', { ignorePureComponents: true }],
-    'react/prop-types': 'off',
-    'react/jsx-indent': ['error', 2],
-    'react/jsx-indent-props': ['error', 2],
-    'react/jsx-filename-extension': ['warn', { extensions: ['.js', '.jsx'] }],
-    'react/no-multi-comp': 'off',
-    'react/no-did-update-set-state': 'off',
-    'jsx-a11y/label-has-for': 'off',
-    'jsx-a11y/no-noninteractive-tabindex': 'off',
-    'jsx-a11y/tabindex-no-positive': 'off',
-    'jsx-a11y/no-autofocus': 'off',
-    'no-mixed-operators': [
-      'error',
-      {
-        groups: [
-          ['*', '/'],
-          ['&', '|', '^', '~', '<<', '>>', '>>>'],
-          ['==', '!=', '===', '!==', '>', '>=', '<', '<='],
-          ['&&', '||'],
-          ['in', 'instanceof'],
-        ],
-        allowSamePrecedence: false,
+    },
+    environment: {
+      browser: true,
+      node: true,
+      es6: true,
+    },
+    plugins: {
+      react: reactPlugin,
+      jest: jestPlugin,
+      import: importPlugin,
+      'jsx-a11y': jsxA11yPlugin,
+    },
+    settings: {
+      react: {
+        version: 'detect', // Automatically detect React version
       },
-    ],
+    },
+    rules: {
+      ...eslintConfigAirbnbBase.rules, // Include Airbnb base rules
+      ...eslintConfigPrettier.rules, // Include Prettier rules
+
+      // Custom rules
+      'react/jsx-key': 'off',
+      'jsx-a11y/click-events-have-key-events': 'off',
+      'jsx-a11y/no-noninteractive-element-to-interactive-role': 'off',
+      'react/destructuring-assignment': 'off',
+      'no-unused-vars': 'off',
+      'react/display-name': 'off',
+      'import/namespace': 'off',
+      'max-len': 'off',
+      'sort-imports': 'off',
+      'import/order': 'off',
+      'import/first': 'off',
+      quotes: ['error', 'single', { allowTemplateLiterals: true }],
+      indent: ['error', 2, { SwitchCase: 1 }],
+      'no-underscore-dangle': 'off',
+      'import/no-extraneous-dependencies': [
+        'error',
+        {
+          devDependencies: true,
+          optionalDependencies: false,
+          peerDependencies: false,
+        },
+      ],
+      'react/prefer-stateless-function': ['warn', { ignorePureComponents: true }],
+      'react/prop-types': 'off',
+      'react/jsx-indent': ['error', 2],
+      'react/jsx-indent-props': ['error', 2],
+      'react/jsx-filename-extension': ['warn', { extensions: ['.js', '.jsx'] }],
+      'react/no-multi-comp': 'off',
+      'react/no-did-update-set-state': 'off',
+      'jsx-a11y/label-has-for': 'off',
+      'jsx-a11y/no-noninteractive-tabindex': 'off',
+      'jsx-a11y/tabindex-no-positive': 'off',
+      'jsx-a11y/no-autofocus': 'off',
+      'no-mixed-operators': [
+        'error',
+        {
+          groups: [
+            ['*', '/'],
+            ['&', '|', '^', '~', '<<', '>>', '>>>'],
+            ['==', '!=', '===', '!==', '>', '>=', '<', '<='],
+            ['&&', '||'],
+            ['in', 'instanceof'],
+          ],
+          allowSamePrecedence: false,
+        },
+      ],
+    },
   },
-};
+];
