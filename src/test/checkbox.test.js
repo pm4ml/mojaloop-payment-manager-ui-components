7c7a1b5f14d2ad5b356d94e84ce8fcab51748bc5
Chris Law <62750125+chris-me-law@users.noreply.github.com>
2024-10-24 14:07:38 +0100

    Update checkbox.test.js

--- src/test/checkbox.test.js
+++ src/test/checkbox.test.js
@@ -4,24 +4,26 @@ import React from 'react';
 
 import Checkbox from '../components/Checkbox';
 
+// Rendering Tests
+
 it('renders the checkbox', () => {
-  const wrapper = shallow(<Checkbox />);
+  const wrapper = shallow(<Checkbox id="checkbox-test" />);
   expect(wrapper.find('div.input-checkbox')).toHaveLength(1);
 });
 
 it('renders the checkbox value', () => {
-  const wrapper = shallow(<Checkbox checked />);
+  const wrapper = shallow(<Checkbox id="checkbox-test" checked />);
   expect(wrapper.find('input[type="checkbox"]').prop('checked')).toBe(true);
 });
 
 it('renders the label', () => {
-  const wrapper = shallow(<Checkbox label="test-checkbox" />);
+  const wrapper = shallow(<Checkbox id="checkbox-test" label="test-checkbox" />);
   expect(wrapper.find('label')).toHaveLength(1);
   expect(wrapper.find('label').text()).toBe('test-checkbox');
 });
 
 it('applies the prop className', () => {
-  const wrapper = shallow(<Checkbox className="test" />);
+  const wrapper = shallow(<Checkbox id="checkbox-test" className="test" />);
   expect(wrapper.find('.test')).toHaveLength(1);
 });
 
@@ -31,21 +33,21 @@ it('renders the prop id', () => {
 });
 
 it('renders the disabled state', () => {
-  const wrapper = shallow(<Checkbox disabled />);
+  const wrapper = shallow(<Checkbox id="checkbox-test" disabled />);
   expect(wrapper.find('input')).toHaveLength(1);
   expect(wrapper.find('input').prop('disabled')).toBe(true);
 });
 
 it('renders the round state', () => {
-  const wrapper = shallow(<Checkbox round />);
+  const wrapper = shallow(<Checkbox id="checkbox-test" round />);
   expect(wrapper.find('label').hasClass('input-checkbox__label--round')).toBeTruthy();
 });
 
-// Events
+// Event Tests
 
 it('triggers onFocus when focused', () => {
   const mockEvent = jest.fn();
-  const wrapper = mount(<Checkbox onFocus={mockEvent} />);
+  const wrapper = mount(<Checkbox id="checkbox-test" onFocus={mockEvent} />);
   expect(mockEvent).not.toHaveBeenCalled();
   wrapper.find('input').simulate('focus');
   expect(mockEvent).toHaveBeenCalled();
@@ -53,7 +55,7 @@ it('triggers onFocus when focused', () => {
 
 it('triggers onBlur when blurred', () => {
   const mockEvent = jest.fn();
-  const wrapper = mount(<Checkbox onBlur={mockEvent} />);
+  const wrapper = mount(<Checkbox id="checkbox-test" onBlur={mockEvent} />);
   expect(mockEvent).not.toHaveBeenCalled();
   wrapper.find('input').simulate('blur');
   expect(mockEvent).toHaveBeenCalled();
@@ -61,15 +63,17 @@ it('triggers onBlur when blurred', () => {
 
 it('triggers onChange when changed', () => {
   const mockEvent = jest.fn();
-  const wrapper = mount(<Checkbox onChange={mockEvent} />);
+  const wrapper = mount(<Checkbox id="checkbox-test" onChange={mockEvent} />);
+  
+  // Simulate a change event on the checkbox input
   expect(mockEvent).not.toHaveBeenCalled();
-  wrapper.find('label').simulate('click');
-  expect(mockEvent).toHaveBeenCalled();
+  wrapper.find('input').simulate('change', { target: { checked: true } });
+  expect(mockEvent).toHaveBeenCalledWith(true);
 });
 
 it('triggers onChange when pressing Enter key', () => {
   const mockEvent = jest.fn();
-  const wrapper = mount(<Checkbox onChange={mockEvent} />);
+  const wrapper = mount(<Checkbox id="checkbox-test" onChange={mockEvent} />);
   expect(mockEvent).not.toHaveBeenCalled();
   wrapper.find('input').simulate('keyDown', { key: 'Enter', keyCode: 13, which: 13 });
   expect(mockEvent).toHaveBeenCalled();
@@ -78,15 +82,15 @@ it('triggers onChange when pressing Enter key', () => {
 
 it('triggers onBlur when blurring', () => {
   const mockEvent = jest.fn();
-  const wrapper = mount(<Checkbox onBlur={mockEvent} />);
+  const wrapper = mount(<Checkbox id="checkbox-test" onBlur={mockEvent} />);
   expect(mockEvent).not.toHaveBeenCalled();
   wrapper.find('input').simulate('blur', { key: 'Tab', keyCode: 9, which: 9 });
   expect(mockEvent).toHaveBeenCalled();
 });
 
-// Snapshot
+// Snapshot Test
 
 it('renders the checkbox correctly when multiple props are set', () => {
-  const wrapper = shallow(<Checkbox checked id="test-id" />);
+  const wrapper = shallow(<Checkbox id="test-id" checked />);
   expect(shallowToJson(wrapper)).toMatchSnapshot();
 });
